cde2e35d98d3440fb690ecc978d23c47a89a1d1f
Song Zheng <song@zheng.club>
2020-03-22 05:20:42 -0700

    add ability to see google map location

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const express = require('express');
-const app = express();
-const assetExercise = require('./src/assetExercise');
-app.use(express.static('public'));
-app.use(express.json());
+const express = require('express')
+const locationRoute = require('./src/location')
+const app = express()
+const assetExercise = require('./src/assetExercise')
+
+app.set('trust proxy', true)
+app.use(express.static('public'))
+app.use(express.json())
 
 app.use('/assetExercise', assetExercise)
+app.use('/location', locationRoute)
 
-app.listen(process.env.PORT || 3020);
+app.listen(process.env.PORT || 3020)
---
+++ src/location.js
@@ -0,0 +1,77 @@
+const express = require('express')
+const geoip = require('geoip-lite')
+const router = express.Router()
+
+const cityToIpMappings = {}
+
+const getIp = (req) => {
+  const ipInfo = req.ip || req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress
+  const ipv4 = ipInfo.split(':').pop()
+  if (ipv4.includes('192.168')) { return '45.30.93.93' }
+  return ipv4
+}
+
+router.use('*', async (req, res, next) => {
+  const ip = getIp(req)
+  const location = geoip.lookup(ip)
+  if (!location) {
+    return res.send('No ip available')
+  }
+
+  const city = location.city ? location.city + ', ' : ''
+  const region = location.region ? location.region + ', ' : ''
+  const cityStr = `${city} ${region} ${location.country}`
+  const cityInfo = cityToIpMappings[cityStr] || {}
+  cityInfo[ip] = cityInfo[ip] || { ...location, ip, cityStr, count: 0 }
+  cityInfo[ip].count += 1
+  req.user = cityInfo[ip]
+  cityInfo[ip] = req.user
+  cityToIpMappings[cityStr] = cityInfo
+  next()
+})
+
+router.get('/', (req, res) => {
+  const sortedCityMappings = Object.entries(cityToIpMappings).sort((city1, city2) => {
+    return Object.keys(city1[1]) - Object.keys(city2[1])
+  })
+  const cityCards = sortedCityMappings.reduce((acc, [cityname, cityInfo]) => {
+    const count = Object.values(cityInfo).reduce((acc, city) => {
+      return city.count
+    }, 0)
+    return acc + `
+    <h2>${cityname} - ${count}</h2>
+    `
+  }, '')
+
+  const ipMappings = cityToIpMappings[req.user.cityStr]
+  const ipMarkers = Object.values(ipMappings).reduce((acc, location) => {
+    return acc + `
+  new google.maps.Marker({
+    position: {lat: ${location.ll[0]}, lng: ${location.ll[1]}},
+    map: map,
+    title: '${location.count} Hits'
+  })
+    `
+  }, '')
+
+  const html = `
+<div id="googleMap" style="width:100%;height:600px;"></div>
+<div>${cityCards}</div>
+
+<script>
+  function myMap() {
+    var mapProp= {
+      center:new google.maps.LatLng(${req.user.ll[0]},${req.user.ll[1]}),
+        zoom:11,
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+    ${ipMarkers}
+  }
+</script>
+
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB29pGpCzE_JGIEMLu1SGIqwoIbc0sHFHo&callback=myMap"></script>
+  `
+  res.send(html)
+})
+
+module.exports = router
