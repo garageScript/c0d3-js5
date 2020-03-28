c23992fd39b1fa4f603dacb1cab7d9f778f3c937
Song Zheng <song@zheng.club>
2020-03-27 18:03:06 -0700

    add security

--- src/command.js
+++ src/command.js
@@ -1,19 +1,27 @@
 const express = require('express')
-// const { exec } = require('child_process')
+const { exec } = require('child_process')
 const router = express.Router()
 
 router.post('/', (req, res) => {
-  return res.json({ output: 'service unavailable' })
-  /*
   const command = req.body.command
-  console.log('command executed', command)
-  exec(command, {}, (err, stdout) => {
+  const argList = command.split(' ')
+  const allowed = [
+    'ls',
+    'cd',
+    'touch',
+    'git',
+    'cat'
+  ]
+  console.log('command is: ', command)
+  if (!allowed.includes(argList[0]) || argList.length > 2) {
+    return res.json({ output: 'command not allowed' })
+  }
+  exec(`${argList[0]} ${argList[1] || ''}`, {}, (err, stdout) => {
     if (err) {
       return res.json({ output: 'error' })
     }
     res.json({ output: stdout || 'your command resulted in no output' })
   })
-  */
 })
 
 module.exports = router
