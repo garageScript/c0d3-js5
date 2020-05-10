b8c77dd8ffed48d063482102467a5aca4b08957b
Song Zheng <song@zheng.club>
2020-05-09 21:11:41 -0700

    lambdas - index page links, url links, and mapping data store

--- src/lambdas.js
+++ src/lambdas.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const fs = require('fs')
+const URL = require('url')
 const path = require('path')
+const { getData, setData } = require('./lib/db')
 const router = express.Router()
 
 const assetPath = 'userUploads/assets'
+
+let mappings = {}
+getData('lambdaMapping').then((data) => {
+  mappings = data || {}
+})
 
 router.get('/api/files', (req, res) => {
   fs.readdir(path.join(__dirname, `../${assetPath}`), (err, allFiles) => {
@@ -24,7 +31,6 @@ router.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../views/lambdas.html'))
 })
 
-const mappings = {}
 router.get('/api/mappings', (req, res) => {
   res.json(mappings)
 })
@@ -41,6 +47,7 @@ router.post('/api/mappings', (req, res) => {
     fullPath: assetPathName,
     fileName: req.body.file
   }
+  setData('lambdaMapping', mappings)
   res.json({
     mappings, body: req.body
   })
@@ -48,7 +55,10 @@ router.post('/api/mappings', (req, res) => {
 
 router.use('/*', (req, res, next) => {
   req.log('lambda originalUrl', req.originalUrl)
-  const route = mappings[req.originalUrl]
+  // eslint-disable-next-line
+  const pathname = URL.parse(req.originalUrl).pathname
+  req.log('lambda path', pathname)
+  const route = mappings[pathname]
   if (!route) {
     req.warn('lambda route not found', req.originalUrl)
     return res.status(400).send('No Lambdas Available')
