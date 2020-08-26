7d2a6085c395d74e36e14316a2d3f2c476d50e29
Song Zheng <song@zheng.club>
2020-08-26 15:31:15 -0700

    unkwn update

--- server.js
+++ server.js
@@ -1,27 +1,28 @@
-const express = require('express')
-const session = require('express-session')
-const { uuid } = require('uuidv4')
-const locationRoute = require('./src/location')
-const authRoute = require('./src/auth')
-const commandRoute = require('./src/command')
-const ocrRoute = require('./src/ocr')
-const chatRoute = require('./src/chat')
-const chatroomRoute = require('./src/chatroom')
-const abtestRoute = require('./src/abtest')
-const todoListRoute = require('./src/todoList')
-const imggen = require('./src/imagegen')
-const js6c1 = require('./src/js6c1')
-const js6c2 = require('./src/js6c2')
-const exampleRoute = require('./src/examples')
-const lambdas = require('./src/lambdas')
-const assetExercise = require('./src/assetExercise')
-const imageAnalysis = require('./src/imageAnalysis')
-const logGen = require('./src/log')
-const app = express()
+const express = require("express");
+const session = require("express-session");
+const { uuid } = require("uuidv4");
+const locationRoute = require("./src/location");
+const authRoute = require("./src/auth");
+const commandRoute = require("./src/command");
+const ocrRoute = require("./src/ocr");
+const chatRoute = require("./src/chat");
+const chatroomRoute = require("./src/chatroom");
+const abtestRoute = require("./src/abtest");
+const todoListRoute = require("./src/todoList");
+const imggen = require("./src/imagegen");
+const { cors } = require("./src/lib/middleware");
+const js6c1 = require("./src/js6c1");
+const js6c2 = require("./src/js6c2");
+const exampleRoute = require("./src/examples");
+const lambdas = require("./src/lambdas");
+const assetExercise = require("./src/assetExercise");
+const imageAnalysis = require("./src/imageAnalysis");
+const logGen = require("./src/log");
+const app = express();
 
-app.set('trust proxy', true)
-app.use(express.static('public'))
-app.use(express.json({ limit: '50mb' }))
+app.set("trust proxy", true);
+app.use(express.static("public"));
+app.use(express.json({ limit: "50mb" }));
 
 // DOCS about session
 // resave:
@@ -30,77 +31,84 @@ app.use(express.json({ limit: '50mb' }))
 //     Choosing false is useful for implementing login sessions,
 //     reducing server storage usage,
 //     or complying with laws that require permission before setting a cookie.
-const FIVE_MINUTES = 1000 * 60 * 5
-app.use(session({
-  secret: 'keyboard cat',
-  resave: true,
-  saveUninitialized: false,
-  cookie: { sameSite: 'none', secure: true, maxAge: FIVE_MINUTES }
-}))
+const FIVE_MINUTES = 1000 * 60 * 5;
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: true,
+    saveUninitialized: false,
+    cookie: { sameSite: "none", secure: true, maxAge: FIVE_MINUTES },
+  })
+);
 
 app.use((req, res, next) => {
-  const sid = uuid()
-  const reqLogger = logGen(null, sid)
-  req.log = reqLogger.log
-  req.warn = reqLogger.warn
-  req.error = reqLogger.error
-  res.set('c0d3-debug-id', sid)
-  next()
-})
-app.use('/assetExercise', assetExercise)
-app.use('/imageAnalysis', imageAnalysis)
-app.use('/location', locationRoute)
-app.use('/auth', authRoute)
-app.use('/commands', commandRoute)
-app.use('/js6c1', js6c1)
-app.use('/js6c2', js6c2)
-app.use('/ocr', ocrRoute)
-app.use('/chat', chatRoute)
-app.use('/chatroom', chatroomRoute)
-app.use('/imggen', imggen)
-app.use('/lambdas', lambdas)
-app.use('/abtest', abtestRoute)
-app.use('/todoList', todoListRoute)
-app.use('/examples', exampleRoute)
+  const sid = uuid();
+  const reqLogger = logGen(null, sid);
+  req.log = reqLogger.log;
+  req.warn = reqLogger.warn;
+  req.error = reqLogger.error;
+  res.set("c0d3-debug-id", sid);
+  next();
+});
+app.use("/assetExercise", assetExercise);
+app.use("/imageAnalysis", imageAnalysis);
+app.use("/location", locationRoute);
+app.use("/auth", authRoute);
+app.use("/commands", commandRoute);
+app.use("/js6c1", js6c1);
+app.options("/js6c2/graphql", cors(), (req, res, next) => {
+  res.end();
+});
+app.use("/js6c2", js6c2);
+app.use("/ocr", ocrRoute);
+app.use("/chat", chatRoute);
+app.use("/chatroom", chatroomRoute);
+app.use("/imggen", imggen);
+app.use("/lambdas", lambdas);
+app.use("/abtest", abtestRoute);
+app.use("/todoList", todoListRoute);
+app.use("/examples", exampleRoute);
 
-const requests = []
-app.get('/jsFile', (req, res) => {
+const requests = [];
+app.get("/jsFile", (req, res) => {
   res.json({
-    total: requests.length, requests
-  })
-})
-app.get('/jsFile.js', (req, res) => {
-  const date = new Date()
+    total: requests.length,
+    requests,
+  });
+});
+app.get("/jsFile.js", (req, res) => {
+  const date = new Date();
   requests.unshift({
     method: req.method,
-    time: date.toTimeString()
-  })
-  res.set('Cache-Control', 'max-age=50')
-  res.set('Expires', '50')
-  res.send('console.log(\'hello zach\')')
-})
+    time: date.toTimeString(),
+  });
+  res.set("Cache-Control", "max-age=50");
+  res.set("Expires", "50");
+  res.send("console.log('hello zach')");
+});
 
-let server
+let server;
 
 const startServer = (port = 3020) => {
   return new Promise((resolve, reject) => {
     server = app.listen(port, function () {
-      resolve(server)
-    })
-    server.on('error', (err) => {
+      resolve(server);
+    });
+    server.on("error", (err) => {
       server.close(() => {
-        reject(err)
-      })
-    })
-  })
-}
+        reject(err);
+      });
+    });
+  });
+};
 
 const stopServer = () => {
   return new Promise((resolve, reject) => {
-    server.close(resolve)
-  })
-}
+    server.close(resolve);
+  });
+};
 
 module.exports = {
-  startServer, stopServer
-}
+  startServer,
+  stopServer,
+};
--- src/lib/middleware.js
+++ src/lib/middleware.js
@@ -1,35 +1,36 @@
-const jwt = require('jsonwebtoken')
-const { getUser } = require('./userlist')
-const middlewares = {}
+const jwt = require("jsonwebtoken");
+const { getUser } = require("./userlist");
+const middlewares = {};
 
 middlewares.setUser = (req, res, next) => {
   if (req.session.username) {
-    req.user = getUser(req.session.username)
+    req.user = getUser(req.session.username);
   }
 
-  const authToken = (req.headers.authorization || '').split(' ')
+  const authToken = (req.headers.authorization || "").split(" ");
   if (authToken) {
-    const data = jwt.decode(authToken.pop()) || {}
+    const data = jwt.decode(authToken.pop()) || {};
     if (data.username) {
-      req.user = getUser(data.username)
+      req.user = getUser(data.username);
     }
   }
 
-  next()
-}
+  next();
+};
 
 middlewares.cors = () => {
   return (req, res, next) => {
+    console.log("res header");
     // Credentials - https://stackoverflow.com/questions/24687313/what-exactly-does-the-access-control-allow-credentials-header-do
-    res.header('Access-Control-Allow-Credentials', true)
-    res.header('Access-Control-Allow-Origin', req.headers.origin)
-    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, PATCH, DELETE') // cors preflight
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Origin", req.headers.origin);
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, PATCH, DELETE"); // cors preflight
     res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept, Credentials'
-    )
-    next()
-  }
-}
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Credentials"
+    );
+    next();
+  };
+};
 
-module.exports = middlewares
+module.exports = middlewares;
