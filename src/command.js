b43c012cc2e50caa9ab628fee1a6a5dc8e57f1f7
Guilherme Gwadera <16023489+ggwadera@users.noreply.github.com>
2021-02-08 17:46:20 -0300

    fix arbitrary command execution exploit
    Switched `exec` to `execFile`, as it prevents arbitrary command execution through shell redirection.

--- src/command.js
+++ src/command.js
@@ -1,23 +1,28 @@
 const express = require('express')
-const { exec } = require('child_process')
+const { execFile } = require('child_process')
 const router = express.Router()
 
+// allowed characters regex
+// only alphanumeric, whitespace, '-', '_', '~', '.' and '/'
+const safe = /^[a-zA-Z0-9-_~\/\.\s]*$/;
+
+const allowed = [
+  'ls',
+  'cd',
+  'touch',
+  'git',
+  'pwd',
+  'cat'
+]
+
 router.post('/', (req, res) => {
-  const command = req.body.command
+  const { command } = req.body
   const argList = command.split(' ')
-  const allowed = [
-    'ls',
-    'cd',
-    'touch',
-    'git',
-    'pwd',
-    'cat'
-  ]
-  req.log('command is: ', command)
-  if (!allowed.includes(argList[0]) || argList.length > 2) {
+  req.log('command is: ', argList[0], argList.slice(1))
+  if (!allowed.includes(argList[0]) || !safe.test(command)) {
     return res.json({ output: 'command not allowed' })
   }
-  exec(`${argList[0]} ${argList[1] || ''}`, {}, (err, stdout) => {
+  execFile(argList[0], argList.slice(1), {}, (err, stdout) => {
     if (err) {
       return res.json({ output: 'error' })
     }
