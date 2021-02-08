7e99760a0755632e28c61cf5d5b1357dac56ff95
Guilherme Gwadera <16023489+ggwadera@users.noreply.github.com>
2021-02-08 18:31:06 -0300

    fix `~` not working in commands

--- src/command.js
+++ src/command.js
@@ -2,9 +2,11 @@ const express = require('express')
 const { execFile } = require('child_process')
 const router = express.Router()
 
+const HOME_PATH = require('os').homedir()
+
 // allowed characters regex
 // only alphanumeric, whitespace, '-', '_', '~', '.' and '/'
-const safe = /^[a-zA-Z0-9-_~\/\.\s]*$/;
+const safe = /^[a-zA-Z0-9-_~/.\s]*$/
 
 const allowed = [
   'ls',
@@ -17,12 +19,13 @@ const allowed = [
 
 router.post('/', (req, res) => {
   const { command } = req.body
-  const argList = command.split(' ')
-  req.log('command is: ', argList[0], argList.slice(1))
+  // have to replace "~" because execFile doesn't do expansion
+  req.log('command is: ', command)
+  const argList = command.replace(/~/g, HOME_PATH).split(' ')
   if (!allowed.includes(argList[0]) || !safe.test(command)) {
     return res.json({ output: 'command not allowed' })
   }
-  execFile(argList[0], argList.slice(1), {}, (err, stdout) => {
+  execFile(argList[0], argList.slice(1), (err, stdout) => {
     if (err) {
       return res.json({ output: 'error' })
     }
