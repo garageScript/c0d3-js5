a3d567fc9ed7d89a819c9832dc68594d98905e5b
Thomas Herzog <Tomrule007@gmail.com>
2021-06-02 18:45:46 -0700

    create fake env file to share private auth secret

---
+++ src/lib/fakeEnv.js
@@ -0,0 +1,5 @@
+// In a real app this would be in a .env file and not included in the repo
+// the .env file gets loaded when the server starts and is accessible via process.env[name]
+module.exports = {
+  JWT_PRIVATE_SECRET: 'reallygreatc0der'
+}
