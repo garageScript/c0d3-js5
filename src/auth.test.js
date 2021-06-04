3ba148509f540a9658ef814767297869adf1d4fe
Thomas Herzog <Tomrule007@gmail.com>
2021-06-03 23:56:05 -0700

    First attempt at decoupling auth testcase

--- src/auth.test.js
+++ src/auth.test.js
@@ -1,50 +1,65 @@
-/* globals describe it jest expect */
+/* globals afterAll beforeAll describe it jest expect */
 const { app } = require('../server')
 const request = require('supertest')
 const jwt = require('jsonwebtoken')
 
-// Prevent actual db writes/reads
+// Prevent actual db writes/reads and seed mock user
 jest.mock('./lib/db', () => ({
-  getData: jest.fn().mockResolvedValue({}),
+  getData: jest.fn().mockResolvedValue({
+    mockUser: {
+      username: 'mockUser',
+      email: 'mock@mock',
+      jwt: 'mockJwt',
+      id: 'whoCares'
+    }
+  }),
   setData: jest.fn().mockReturnValue(null)
 }))
 
 describe('auth (js5/p6)', () => {
-  const mockUser = { username: 'tom', email: 'tom@tom' }
+  const mockUser = { username: 'mockUser', email: 'mock@mock', jwt: 'mockJwt' }
 
   const validResponse = expect.objectContaining({
     ...mockUser,
     jwt: expect.any(String)
   })
 
-  // set real jwt when creating user
-  let realJwt
-  it('creating a user return a jwt', async () => {
-    const response = await request(app)
-      .post('/auth/api/users')
-      .send({
-        ...mockUser,
-        password: 'anything'
-      })
+  beforeAll(() => {
+    jest.spyOn(jwt, 'verify').mockImplementation((jwt) => {
+      if (jwt === mockUser.jwt) return mockUser
+      throw Error('BAD JWT')
+    })
+    jest.spyOn(jwt, 'decode').mockReturnValue(mockUser)
+  })
 
-    expect(response.body).toEqual(validResponse)
-    realJwt = response.body.jwt
+  afterAll(() => {
+    jest.restoreAllMocks()
+  })
+
+  it('creating a new user return a jwt', async () => {
+    const response = await request(app).post('/auth/api/users').send({
+      username: 'tom',
+      email: 'tom@tom',
+      password: 'anything'
+    })
+
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        jwt: expect.any(String)
+      })
+    )
   })
   it('can get session with jwt', async () => {
     const response = await request(app)
       .get('/auth/api/session')
-      .set('Authorization', `Bearer ${realJwt}`)
-
+      .set('Authorization', `Bearer ${'mockJwt'}`)
     expect(response.body).toEqual(validResponse)
   })
   it('should not return session with fake jwt', async () => {
-    const fakeJwt = await jwt.sign({ username: 'tom' }, 'just_a_random_key')
     const response = await request(app)
       .get('/auth/api/session')
-      .set('Authorization', `Bearer ${fakeJwt}`)
+      .set('Authorization', `Bearer ${'BAD_JWT'}`)
 
-    expect(response.body).not.toEqual(
-      expect.objectContaining({ ...mockUser, jwt: realJwt })
-    )
+    expect(response.body).not.toEqual(validResponse)
   })
 })
